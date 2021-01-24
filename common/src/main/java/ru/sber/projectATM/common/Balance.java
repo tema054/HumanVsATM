1f1ec5e1a6228536d4d72293601f94ab8f2e321c
tema054 <tema054@inbox.ru>
2021-01-24 18:53:33 +0300

    + spring boot part 2

--- common/src/main/java/ru/sber/projectATM/common/Balance.java
+++ common/src/main/java/ru/sber/projectATM/common/Balance.java
@@ -7,7 +7,7 @@ import ru.sber.projectATM.common.handbook.*;
 
 @Value
 @ToString
-public class Balance {
+public class Balance implements java.io.Serializable {
     private Double amount;
     private Currency currency;
 
